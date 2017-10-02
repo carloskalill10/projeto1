0a0c34f125a07f63e35b6483f3e462834dae6596
Carlos <carloskalill10@gmail.com>
2017-10-02 10:07:08 -0400

    captura dos dados dos sensores e interface

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<center> <h2> <b> VIZUALIZAÇÃO DOS VALORES DE PRESSÃO E VAZÃO </b> </h2> </center>
+<table align="center">
+	<tr>
+		<td>
+			<a href=vizualizapressao.php ><input type="button" name="pressao" value="pressão"> </a>
+		</td>
+		<td>
+			<a href=vizualizavazao.php ><input type="button" name="vazao" value="vazão"> </a>
+		</td>
+	</tr>
+</table>
+</body>
+</html>
---
+++ vizualizavazao.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+<center> <h2> <b>Dados de Vazão dos sensores </b> </h2> </center>
+
+
+
+<?php
+	$mysqli = new mysqli("localhost","root", "", "projeto1"); //connect on database;
+
+	if (mysqli_connect_errno()) trigger_error(mysqli_connect_error());
+	$query= "select * from vazao order by data asc";
+	$dados = $mysqli->query($query);
+	echo "<center>";
+	echo "<table border=1 width=40%>";
+	echo "<tr>";
+	echo "<td><b> id  </b> </td> ";
+	echo "<td><b> tag </b> </td> ";
+	echo "<td><b> valor </b> </td> ";
+	echo "<td><b> data / hora </b> </td> ";
+	echo "</tr>";
+
+	while ($cadastro = $dados -> fetch_array ()) {
+
+		echo "<tr>";
+		echo "<td>".$cadastro ["id"]."</td>";
+		echo "<td>".$cadastro ["tag"]."</td>";
+		echo "<td>".$cadastro ["valor"]."</td>";
+		echo "<td>".$cadastro ["data"]."</td>";	
+		
+	}
+echo "</table>";
+echo "</center>";
+?>
+
+</body>
+</html>
